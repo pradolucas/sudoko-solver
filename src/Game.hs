44575b542f33d84372d884c8f5c0880037eba7e8
pradolucas <santos.lucasprado@gmail.com>
2023-08-27 15:48:33 -0300

    [fix] random initialization of grid respect rules

--- src/Game.hs
+++ src/Game.hs
@@ -13,7 +13,8 @@ module Game
   -- )
    where
 import Control.Monad.Random
-
+    ( fromList, evalRand, mkStdGen, RandomGen )
+import Data.Digest.Murmur32 ( asWord32, hash32 ) 
 
 type Grid = [[Int]]
 
@@ -23,9 +24,10 @@ data SudokuGame = SudokuGame
   , finished :: Bool
   }
 
+-- Params
 
 gridSize :: Int
-gridSize = 9
+gridSize = 4
 
 screenWidth, screenHeight :: Float
 screenWidth = 400
@@ -39,31 +41,98 @@ middleOfGridX, middleOfGridY :: Float
 middleOfGridX = screenWidth * 0.5
 middleOfGridY = screenHeight * 0.5
 
-weightedValues :: [(Int, Rational)]
-weightedValues = (0, 10) : [(i, 1) | i<-[1..(gridSize-1)]] 
+
+-- Randomness
+
+weightedDistribution :: [(Int, Rational)]
+weightedDistribution = (0, 5) : [(i, 1) | i<-[1..(gridSize-1)]]
 -- a dificuldade vai será determinada pela probabilidade de 0
 
-weightedList :: RandomGen g => g -> [(a, Rational)] -> [a]
-weightedList gen weights = evalRand m gen
+customDistribution :: RandomGen g => g -> [(a, Rational)] -> [a]
+customDistribution gen weights = evalRand m gen
     where m = sequence . repeat . fromList $ weights
--- run validation of the initial 
 
-gridify :: Int -> [a] -> [[a]]
-gridify _ [] = []
-gridify n xs = take n xs : gridify n (drop n xs)
+hashSeed :: (Num b, Show a1, Show a2) => a1 -> a2 -> b
+hashSeed row col = fromIntegral $ asWord32 $ hash32 (show row ++ "," ++ show col)
 
+-- Validate 
+
+isGridValid :: Grid -> [Bool]
+isGridValid g = [isFinished (i,j) (g !! i !! j) g
+                | i<-[0..(gridSize-1)], j<-[0..(gridSize-1)]]
+
+-- Double check, apagar posteriormente
+isFinished :: Eq a => (Int, Int) -> a -> [[a]] -> Bool
+isFinished (x,y) val gr = 
+  elementAppearsOnce val getRow -- checkLinha
+  && elementAppearsOnce val getColumn -- checkColuna
+  && elementAppearsOnce val getRegion -- checkBloco
+  where
+    elementAppearsOnce element list = length (filter (== element) list) == 1
+    getRow = gr !! x
+    getColumn = map (!! y) gr
+    getRegion = [gr !! i !! j | i<-[blockLowerBoundX .. blockUpperBoundX],
+                     j<- [blockLowerBoundY .. blockUpperBoundY]]
+    blockSize = truncate . sqrt $ fromIntegral gridSize
+    blockLowerBoundX = blockSize * (x `div` blockSize)
+    blockUpperBoundX = blockLowerBoundX + (blockSize-1)
+    blockLowerBoundY = blockSize*(y `div` blockSize)
+    blockUpperBoundY = blockLowerBoundY + (blockSize-1)
+
+-- Somente válido durante a inserção
+isCellValid :: (Int, Int) -> Int -> Grid -> Bool
+isCellValid _ 0 _ = True
+isCellValid (x, y) val g =
+  notElem val getRow -- checkLinha
+  && notElem val getColumn -- checkColuna
+  && notElem val getRegion -- checkBloco
+  where
+    getRow = g !! x
+    getColumn = map (!! y) g
+    getRegion =
+      [g !! i !! j | i<-[blockLowerBoundX .. blockUpperBoundX],
+                     j<- [blockLowerBoundY .. blockUpperBoundY]]
+      where
+        blockSize = truncate . sqrt $ fromIntegral gridSize
+        blockLowerBoundX = blockSize * (x `div` blockSize)
+        blockUpperBoundX = blockLowerBoundX + (blockSize-1)
+        blockLowerBoundY = blockSize*(y `div` blockSize)
+        blockUpperBoundY = blockLowerBoundY + (blockSize-1)
+
+
+-- Update State
+
+updateGrid :: (Int, Int) -> Int -> Grid -> Grid
+updateGrid (x, y) val g =
+  take x g ++ [replaceAtIndex y val (g !! x)] ++ drop (x + 1) g
+
+replaceAtIndex :: Int -> a -> [a] -> [a]
+replaceAtIndex n newVal xs = take n xs ++ [newVal] ++ drop (n + 1) xs
+
+fillCellRandomValue :: (Int, Int) -> Int -> Grid -> Grid
+fillCellRandomValue (i,j) seed g
+  | isCellValid (i,j) val g = updateGrid (i,j) val g
+  | otherwise               = fillCellRandomValue (i,j) (hashSeed seed 1) g
+  where
+    val = head $ take 1 $ customDistribution (mkStdGen seed) weightedDistribution
+
+
+runFillGrid :: Grid -> Grid
+runFillGrid g = foldl (\acc (i, j) -> fillCellRandomValue (i,j) (hashSeed i j) acc ) g elementOrder
+  where
+    elementOrder = [(r, c) | c <- [0 .. gridSize - 1], r <- [0 .. gridSize - 1]]
+
+-- Garantir que tem solução, senão chamar novamente
 initialGrid :: Grid
-initialGrid = gridify gridSize $
-              take (gridSize*gridSize) $
-              weightedList (mkStdGen 2) weightedValues
--- initialGrid = replicate gridSize (replicate gridSize 0)
--- initialGrid =  [ [0, 3, 0, 0]
---   , [0, 0, 2, 0]
---   , [0, 0, 0, 1]
---   , [0, 0, 0, 0]
---   ]
+initialGrid = runFillGrid $ replicate gridSize (replicate gridSize 0)
 
 
 initialGame :: SudokuGame
 initialGame = SudokuGame initialGrid Nothing False
+
+
+
+-- gridify :: Int -> [a] -> [[a]]
+-- gridify _ [] = []
+-- gridify n xs = take n xs : gridify n (drop n xs)
 
--- src/Logic.hs
+++ src/Logic.hs
@@ -1,12 +1,25 @@
 module Logic where
 
-import Game ( SudokuGame(..), Grid, initialGame, middleOfGridX, cellWidth, middleOfGridY, cellHeight, gridSize )
+
 import Graphics.Gloss.Interface.IO.Game
     ( Key(Char, MouseButton, SpecialKey),
       Event(EventKey),
       KeyState(Down),
       MouseButton(LeftButton),
       SpecialKey(KeySpace) )
+import Game
+    ( SudokuGame(..),
+      Grid,
+      gridSize,
+      cellWidth,
+      cellHeight,
+      middleOfGridX,
+      middleOfGridY,
+      isCellValid,
+      updateGrid,
+      initialGame,
+      isGridValid )
+
 
 
 handleInput :: Event -> SudokuGame -> SudokuGame
@@ -17,7 +30,9 @@ handleInput (EventKey (MouseButton LeftButton) Down _ mouse) game = case getClic
 -- Prenchimento da celula
 handleInput (EventKey (Char c) Down _ _) game@SudokuGame{ selectedCell = Just (x, y), grid = oldGrid }
   | c >= '1' && c <= '9' && isCellValid (x, y) (read [c]) oldGrid =
-      game { grid = updateGrid (x, y) (read [c]) oldGrid, selectedCell = Nothing, finished = checkFinishedGrid (updateGrid (x, y) (read [c]) oldGrid) }
+      game { grid = updateGrid (x, y) (read [c]) oldGrid,
+             selectedCell = Nothing,
+             finished = checkFinishedGrid (updateGrid (x, y) (read [c]) oldGrid) }
 
 -- Apagar valor anterior, só toma caso tenha uma selectedCell = Just
 handleInput (EventKey (SpecialKey KeySpace) Down _ _) game@SudokuGame{ selectedCell = Just (x, y) } =
@@ -36,32 +51,9 @@ getClickedCell (x, y)
   where
     x' = (x + (middleOfGridX + cellWidth/2)) / cellWidth
     y' = (y - (middleOfGridY + cellHeight/2)) / (-cellHeight)
--- tornar esse valor 50 baseado no gridSize
-isCellValid :: (Int, Int) -> Int -> Grid -> Bool
-isCellValid (x, y) val g =
-  notElem val getRow -- checkLinha
-  && notElem val getColumn -- checkColuna
-  && notElem val getRegion -- checkBloco
-  where
-    getRow = g !! x
-    getColumn = map (!! y) g
-    getRegion =
-      [g !! i !! j | i<-[blockLowerBoundX .. blockUpperBoundX],
-                     j<- [blockLowerBoundY .. blockUpperBoundY]]
-      where
-        blockSize = truncate . sqrt $ fromIntegral gridSize
-        blockLowerBoundX = blockSize * (x `div` blockSize)
-        blockUpperBoundX = blockLowerBoundX + (blockSize-1)
-        blockLowerBoundY = blockSize*(y `div` blockSize)
-        blockUpperBoundY = blockLowerBoundY + (blockSize-1)
-
-
-updateGrid :: (Int, Int) -> Int -> Grid -> Grid
-updateGrid (x, y) val g =
-  take x g ++ [replaceAtIndex y val (g !! x)] ++ drop (x + 1) g
-
-replaceAtIndex :: Int -> a -> [a] -> [a]
-replaceAtIndex n newVal xs = take n xs ++ [newVal] ++ drop (n + 1) xs
 
 checkFinishedGrid :: Grid -> Bool
-checkFinishedGrid = all (notElem 0)
+checkFinishedGrid g = and (isGridValid g)
+-- checkFinishedGrid = all (notElem 0)
+
+
